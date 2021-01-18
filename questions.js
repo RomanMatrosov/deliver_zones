ad3bc0023161889100f4d7f9cd02f6353e10fc74
RomanMatrosov <38510244+RomanMatrosov@users.noreply.github.com>
2021-01-18 21:56:02 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,13 @@
+Как создавать полигоны в ЯМ?
+
+Как поймать клик по полигону на карте ЯМ?
+
+Как менять цену в корзине в зависимости от выбранного полигона (зоны доставки)?
+
+https://yandex.ru/dev/maps/archive/doc/jsapi/2.0/ref/reference/Polygon.html
+
+https://yandex.ru/maps/50/perm/?ll=56.231375%2C57.999411&mode=usermaps&source=constructorLink&um=constructor%3A5283554a2df4f84e872b8336024efd8d100b66c7388cd1dee6d1bb11409f8e1d&z=12
+
+https://www.pblog-perm.ru/
+
+http://webmap-blog.ru/bez-rubriki/risuem-mnogougolnik-na-karte-s-pomoshhyu-api-yandeks-kart-2-x
